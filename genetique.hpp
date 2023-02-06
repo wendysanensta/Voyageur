defec4b46df36cf3fcc5cd1b0164314c82bc19c5
Wendy San <wendy.san@ensta-paris.fr>
2023-02-06 20:30:31 +0100

    On branch master  Changes to be committed: 	new file:   genetique.hpp

---
+++ genetique.hpp
@@ -0,0 +1,15 @@
+#ifndef GENETIQUE_HPP_INCLUDED
+#define GENETIQUE_HPP_INCLUDED
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <utility>
+
+class Tournee
+//Fonction d'adaptation, créer une classe "tournee" et lui
+
+//Génération de nouveaux individus
+
+tournee
+
+#endif
