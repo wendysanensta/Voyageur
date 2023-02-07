95a5031b09896e5a2aa59b3044ed99ef3fc8104d
Wendy San <wendy.san@ensta-paris.fr>
2023-02-07 11:11:51 +0100

    Modifications genetique.hpp

--- genetique.hpp
+++ genetique.hpp
@@ -10,21 +10,28 @@
 class Individu
 {
     public :
-    virtual double adaptation(Individu& i) const;
+    virtual double adaptation(Individu& i) const = 0;
 };
 
 class Population
 {
-
+    public :
+    Vector<Individu> p;
 };
 
 class Chemin : public Individu
 {
     public :
+    Vector<Ville> c;
+    double adaptation(Chemin& tournee) const;
 
 };
 
 //Génération de nouveaux individus
  Tournee hybridation
+
+ // on génère un chemin aléatoire, pas forcément licite (sa fonction d'adaptation est égale à l'infini) On la fait muter :
+ // si les mutants sont de fonction d'adaptation infinie, alors on en choisit une aléatoirement, jusqu'à ce qu'on tombe sur une ville d'adaptation finie
+// utiliser la fonction min de la STL et l'infini
 
 #endif
