350aa34f5445f13eb4f79231283fd23faa8ab6fd
KantSchiele <ahornung@noos.fr>
2023-02-21 11:04:18 +0100

    Modif genetique.hpp et cpp

--- genetique.cpp
+++ genetique.cpp
@@ -5,3 +5,103 @@
 #include "projet.hpp"
 #include "genetique.hpp"
 
+//la fonction adaptation renvoie la longueur du chemin ou +inf s'il n'existe pas.
+Chemin::double adaptation()
+{
+    c = this*;
+    int n = c.ind.size();
+    double adapt;
+    for (int i=0; i<n-1,i++)
+    {
+        if (adj[c[i][c[i+1]]==-1)
+        {
+            int a = std::numeric_limits<HUGE_VAL>::infinity();
+            return a;
+        };
+        else
+        {
+            adapt+ = distance[c[i]][c[i+1]];
+        }
+    }
+    return adapt;
+}
+
+
+void hybridation(const Individu& c1, const Individu& c2, Individu& ij, Individu& ji)
+{
+    int n = c1.ind.size();
+    int l = rand()%n + 1 ; //indice d'hybridation
+    //hybridation
+    for (int i=0; i<n; i++)
+    {
+        if (i>l)
+        {
+            ij.ind[i] = c1.ind[i];
+            ji.ind[i] = c2.ind[j];
+        }
+        else
+        {
+            ij.ind[i] = c2.ind[i];
+            ji.ind[i] = c1.ind[j];
+        }
+    }
+
+}
+
+
+void mutation(const Individu& c, Individu& c_mute)
+{
+    int n = c1.ind.size();
+    int k = rand()%n +1;
+    int l = rand()%n +1;
+    c_mute = c;
+    c_mute.ind[k] = c.ind[l];
+    c_mute.ind[l] = c.ind[k];
+}
+
+Individu mutation_flip(const Individu& c)
+{
+    int n = c1.ind.size();
+    int k = rand()%n +1;
+    int l = rand()%n +1;
+    Chemin c_mute = c;
+    c_mute.ind[k] = c.ind[l];
+    c_mute.ind[k-1] = c.ind[l-1];
+    c_mute.ind[l] = c.ind[k];
+    c_mute.ind[l-1] = c.ind[k-1];
+    return c_mute;
+}
+
+Population reproduction(const Population& p, int n)
+{
+    Population<int> p_tilde1;
+    Population<int> p_tilde2;
+    //vecteurs qui nous donnent l'adaptation des individus adaptes ( < infini) et un autre vecteur qui nous donne leurs indices
+    vector<double> adapt;
+    vector<int> indices
+    int l = p.p.size();
+
+    //calcul de l'adaptation de chaque individu, en supprimant les individus non adaptes
+    for (int i=0;i<l;i++)
+    {
+        double a = adaptation(p.pi[i]);
+        if (a<std::numeric_limits<HUGE_VAL>::infinity())
+        {
+            adapt.pushback(a);
+            indices.pushback(i);
+        }
+    }
+
+    //selection de n individus
+    int c = 0; //compteur
+    while((c!=n)&(adapt.size()!=0))
+    {
+        int k = min_element(adapt.begin(),adapt.end());
+        adapt.pop(k);
+        p_tilde1.p.pushback(k);
+        c = c +1;
+    }
+
+    for
+
+}
--- genetique.hpp
+++ genetique.hpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <limits>
+#include <random>
+#include <bits/stdc++.h>
 #include "projet.hpp"
 
 template <typename T>
@@ -14,15 +17,40 @@ class Individu
     public :
     Individu();   //constructeur
 
+    int size();
+    void push_back(const T&);
+    T operator[] (int i)  { return ind[i]; };
+
     virtual double adaptation () const = 0;
 };
+
+template <typename T>
+int Individu<T>::size() {
+    return *this.ind.size();
+}
+
+template <typename T>
+void Individu<T>::push_back(const T& x) {
+    return *this.ind.push_back(x);
+}
+
+
 
 template<typename T>
 class Population
 {
     public :
     vector<Individu<T>> p;
+
+    Population();
 };
+
+template<typename T>
+Population::Population(){
+
+
+    }
+}
 
 class Chemin : public Individu<Ville>
 {
@@ -38,5 +66,7 @@ class Chemin : public Individu<Ville>
  // on génère un chemin aléatoire, pas forcément licite (sa fonction d'adaptation est égale à l'infini) On la fait muter :
  // si les mutants sont de fonction d'adaptation infinie, alors on en choisit une aléatoirement, jusqu'à ce qu'on tombe sur une ville d'adaptation finie
 // utiliser la fonction min de la STL et l'infini
+
+
 
 #endif
